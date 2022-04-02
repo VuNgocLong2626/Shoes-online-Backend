10f7466cdd7fc118db5209d034b1b23958934453
long <long.speed00@gmail.com>
2022-04-02 11:58:15 +0700

    fix all product add number size

--- app/db/repositories/size_quantity/get_quantity_by_id_product_detail.py
+++ app/db/repositories/size_quantity/get_quantity_by_id_product_detail.py
@@ -1,7 +1,7 @@
 from sqlalchemy import false
 from app.db.database import SessionLocal
 from app.models.schemas.size_quatity import SizeQuantityCreate
-from app.db.tables import SizeQuantity
+from app.db.tables import SizeQuantity, Size
 
 
 db = SessionLocal()
@@ -9,5 +9,5 @@ db = SessionLocal()
 
 def get_quantity_by_id_product_detail(id_product_detail: int):
     db.close()
-    respon = db.query(SizeQuantity).filter(SizeQuantity.id_product_detail == id_product_detail).all()
+    respon = db.query(SizeQuantity, Size.size_number).join(Size).filter(SizeQuantity.id_product_detail == id_product_detail).all()
     return respon
