53399545be8f0cecad3b5d40d5fd4acc8978e969
long <long.speed00@gmail.com>
2022-04-02 09:29:35 +0700

    get url image

--- app/api/routers/file.py
+++ app/api/routers/file.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from typing import List, Optional
 from app.services.file import ImageService
 from app.models.schemas import product_detail as _File_schemas
-
+import base64
 
 router = APIRouter(
     prefix="/file",
@@ -13,3 +13,7 @@ router = APIRouter(
 @router.get("/")
 async def get_file_image(image_path: str):
     return ImageService.get_image(image_path)
+
+@router.get("/kkkkkkkkk")
+async def get_file_image(image_path: str, request: Request):
+    return request.url._url.replace("kkkkkkkkk","")
