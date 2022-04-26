0af567ea6afd6068063b767cb3423e3eb3eeb2a9
long <long.speed00@gmail.com>
2022-04-26 17:48:28 +0700

    FIX API get all admin

--- app/db/repositories/admin/get_all_admin.py
+++ app/db/repositories/admin/get_all_admin.py
@@ -1,13 +1,13 @@
 from sqlalchemy import false
 from app.db.database import SessionLocal
 from app.models.schemas.user import UserLogin
-from app.db.tables import User, Info
+from app.db.tables import User, Info, Permission
 
 
 db = SessionLocal()
 
 
 def get_all_admin():
-    respon = db.query(User.account, Info.full_name).join(Info).filter(User.id_permission.in_((1,2))).all()
+    respon = db.query(User.account, Info.full_name, Permission.name).join(Info).join(Permission).filter(User.id_permission.in_((1,2))).all()
     db.close()
     return respon
